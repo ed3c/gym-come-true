0db2bab8b6319eb14c4b7ff7c4d9d242f2c9d888
ed3c <mcnum01@gmail.com>
2026-08-15 14:36:48 +0800

    build: add KMP project settings

---
+++ settings.gradle.kts
@@ -0,0 +1,40 @@
+import org.gradle.api.initialization.resolve.RepositoriesMode
+
+rootProject.name = "gym-come-true"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":shared")
+include(":androidApp")
+include(":webApp")
